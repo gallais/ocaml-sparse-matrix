90c4828af6aa916f42eb41d0df6761f6cbe307ec
gallais <guillaume.allais@ens-lyon.org>
2014-07-04 23:06:24 +0100

    [ cleaning ] Getting rid of the useless main.ml

--- main.ml
+++
@@ -1,8 +0,0 @@
-open Ring
-open Index
-open SparseMatrix
-open Testable
-open Test
-
-module TestInt   = TestSM (IndexInt) (RingInt)   (TestableInt)
-module TestFloat = TestSM (IndexInt) (RingFloat) (TestableFloat)
